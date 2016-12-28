2cb7581a4d1830705e693d328a8101462f7d0dd2
Enrico Benedetti <enrico_benedetti@yahoo.com>
2016-12-28 21:46:04 +0100

    InMemoryFile added check for read access in content.

--- src/main/scala/org/enricobn/vfs/inmemory/InMemoryFile.scala
+++ src/main/scala/org/enricobn/vfs/inmemory/InMemoryFile.scala
@@ -6,17 +6,23 @@ import org.enricobn.vfs.IOError._
 /**
   * Created by enrico on 12/2/16.
   */
+
 class InMemoryFile private[inmemory] (usersManager: VirtualUsersManager, parent: InMemoryFolder, name: String)
 extends InMemoryNode(usersManager, parent, name) with VirtualFile {
   private var _content: AnyRef = ""
 
-  final def content = Right(_content)
+  final def content =
+    if (!usersManager.checkReadAccess(this)) {
+      "Access denied.".ioErrorE
+    } else {
+        Right(_content)
+    }
 
-  final def content_=(content: AnyRef) = {
+  final def content_=(content: AnyRef) =
     if (!usersManager.checkWriteAccess(this)) {
       "Access denied.".ioErrorE
+    } else {
+      Right(this._content = content)
     }
-    Right(this._content = content)
-  }
 
 }
